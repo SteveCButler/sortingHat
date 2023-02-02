a56cf32db8622fee8501e7c00b4b16576a27ae63
Steve Butler <butlersteve.c@gmail.com>
2023-02-01 20:58:18 -0600

    refactor code into studentCards and armyCards.  Sort New Students list by house.  Morty's Army remains hidden until student is expelled.

--- scripts/main.js
+++ scripts/main.js
@@ -3,115 +3,96 @@ const students = [
     id: 1,
     name: "Harry",
     house: "Gryffindor",
-    expelled: false,
   },
   {
     id: 2,
     name: "Ron",
     house: "Gryffindor",
-    expelled: false,
   },
   {
     id: 3,
     name: "Neville",
     house: "Gryffindor",
-    expelled: false,
   },
   {
     id: 4,
     name: "Dean",
     house: "Gryffindor",
-    expelled: false,
   },
   {
     id: 5,
     name: "Seamus",
     house: "Gryffindor",
-    expelled: false,
   },
   {
     id: 6,
     name: "Hermoine",
     house: "Gryffindor",
-    expelled: false,
   },
   {
     id: 7,
     name: "Lavender",
     house: "Gryffindor",
-    expelled: false,
   },
   {
     id: 8,
     name: "Parvati",
     house: "Gryffindor",
-    expelled: false,
   },
   {
     id: 9,
     name: "Ernie",
     house: "Hufflepuff",
-    expelled: false,
   },
   {
     id: 10,
     name: "Justin",
     house: "Hufflepuff",
-    expelled: false,
   },
   {
     id: 11,
     name: "Hannah",
     house: "Hufflepuff",
-    expelled: false,
   },
   {
     id: 12,
     name: "Susan",
     house: "Hufflepuff",
-    expelled: false,
   },
   {
     id: 13,
     name: "Terry",
     house: "Ravenclaw",
-    expelled: false,
   },
   {
     id: 14,
     name: "Mandy",
     house: "Ravenclaw",
-    expelled: false,
   },
   {
     id: 15,
     name: "Michael",
     house: "Ravenclaw",
-    expelled: false,
   },
   {
     id: 16,
     name: "Anthony",
     house: "Ravenclaw",
-    expelled: false,
   },
   {
     id: 17,
     name: "Padma",
     house: "Ravenclaw",
-    expelled: false,
   },
   {
     id: 18,
     name: "Lisa",
     house: "Ravenclaw",
-    expelled: false,
   },
   {
     id: 19,
-    name: "Draco",
+    name: "Jimbo",
     house: "Slytherin",
-    expelled: false,
   },
   {
     id: 20,
@@ -123,43 +104,36 @@ const students = [
     id: 21,
     name: "Vincent",
     house: "Slytherin",
-    expelled: false,
   },
   {
     id: 22,
     name: "Gregory",
     house: "Slytherin",
-    expelled: false,
   },
   {
     id: 23,
     name: "Blaise",
     house: "Slytherin",
-    expelled: false,
   },
   {
     id: 24,
     name: "Theodore",
     house: "Slytherin",
-    expelled: false,
   },
   {
     id: 25,
     name: "Pansy",
     house: "Slytherin",
-    expelled: false,
   },
   {
     id: 26,
     name: "Millicent",
     house: "Slytherin",
-    expelled: false,
   },
   {
     id: 27,
     name: "Daphne",
     house: "Slytherin",
-    expelled: false,
   },
 ];
 
@@ -225,21 +199,34 @@ const headerContent = () => {
     </div>
   </div>`;
 
-  console.log(htmlString);
-
   renderToDom("#headerContent", htmlString);
 };
 
-// ### Create Cards
+// ### CREATE ARMY
+const createArmy = (array) => {
+  let htmlString = "";
+
+  array.forEach((student) => {
+    htmlString += `<div style="height: 200px; min-width: 200px;" class="card w-25 d-flex flex-column text-center ">
+  <img src="assets/images/death_eaters_wbst.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+       <p class="card-text">${student.name} joined <br/> Morty's Army</p>
+    
+  </div>
+</div>`;
+  });
+
+  renderToDom("#mortysArmyContainer", htmlString);
+};
+
+// ### CREATE STUDENT CARDS
 const createCard = (array) => {
-  let activeStudent = "";
-  let expelledStudent = "";
+  array.sort((a, b) => (a.house > b.house ? 1 : -1));
   let htmlString = "";
 
   //for (const student of array) {
   array.forEach((student) => {
-    if (!student.expelled) {
-      activeStudent += ` <div style="height: 200px; min-width: 200px;" class="card w-25 d-flex flex-row text-center ">
+    htmlString += ` <div style="height: 200px; min-width: 200px;" class="card w-25 d-flex flex-row text-center ">
       <div class="color--${student.house}1"></div>
       <div class="color--${student.house}2"></div>
       <div class="card-body">
@@ -250,41 +237,25 @@ const createCard = (array) => {
      
    </div>
    `;
-    } else {
-      expelledStudent += `<div style="height: 200px; min-width: 200px;" class="card w-25 d-flex flex-column text-center ">
-  <img src="assets/images/death_eaters_wbst.webp" class="card-img-top" alt="...">
-  <div class="card-body">
-       <p class="card-text">${student.name} joined <br/> Morty's Army</p>
-    
-  </div>
-</div>`;
-    }
   });
 
-  if (activeStudent.length > 0) {
-    htmlString = activeStudent;
-    renderToDom("#firstYearsContainer", htmlString);
-  }
-  if (expelledStudent.length > 0) {
-    htmlString = expelledStudent;
-    renderToDom("#mortysArmyContainer", htmlString);
-  }
+  renderToDom("#firstYearsContainer", htmlString);
 };
 
 // ### New Student - SORT
 const sortStudent = (event) => {
   event.preventDefault();
   const studentName = document.querySelector("#inputStudent");
-  const randomSelect = Math.floor(Math.random() * 4);
+  const randomSelect = Math.floor(Math.random() * 40);
   let randomHouse = "";
-  console.log(event.target.value);
-  if (randomSelect === 1) {
+
+  if (randomSelect <= 10) {
     randomHouse = "Gryffindor";
-  } else if (randomSelect === 2) {
+  } else if (randomSelect > 10 && randomSelect <= 20) {
     randomHouse = "Hufflepuff";
-  } else if (randomSelect === 3) {
+  } else if (randomSelect > 20 && randomSelect <= 30) {
     randomHouse = "Ravenclaw";
-  } else if (randomSelect === 0) {
+  } else if (randomSelect > 30 && randomSelect <= 40) {
     randomHouse = "Slytherin";
   }
 
@@ -305,7 +276,7 @@ const eventListenerIntialize = () => {
   // ### SHOW HIDE FORM
   const newStudentButton = document.querySelector("#newStudentButton");
   // *** A way to toggle a class on and off on an element ***
-  newStudentButton.addEventListener("click", (event) => {
+  newStudentButton.addEventListener("click", () => {
     const toggleShow = document.querySelector(".hideForm");
     toggleShow.classList.toggle("showForm");
   });
@@ -345,24 +316,30 @@ const eventListenerIntialize = () => {
 
   //#### EXPEL STUDENT
   const expelled = document.querySelector("#firstYearsContainer");
-
+  const army = [];
   expelled.addEventListener("click", (event) => {
+    const toggleArmy = document.querySelector(".hideExpelled");
+    toggleArmy.classList.add("showExpelled");
+
     if (event.target.id.includes("expel")) {
       const [, studentId] = event.target.id.split("--");
 
       const expelStudent = students.findIndex(
         (student) => student.id === Number(studentId)
       );
-      students[expelStudent].expelled = true;
+      let expelled = students.splice(expelStudent, 1);
+      army.push(expelled[0]);
     }
+    createArmy(army);
     createCard(students);
   });
 };
 
 const appStart = () => {
+  createCard(students);
+  // DISPLAY HEADER CONTENT
   headerContent();
   eventListenerIntialize();
-  createCard(students);
 };
 
 appStart();
